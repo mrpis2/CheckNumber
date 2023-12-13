42095ed9f13713b4cae3be9fbe873a9128546f02
mrpis2 <warhammerprogamer@gmail.com>
2023-12-13 22:47:43 +0700

    checkNumber

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        uint number;
+        Console.WriteLine("Nhap so tu ban phim");
+        number = uint.Parse(Console.ReadLine());
+
+        if (number < 2)
+        {
+            Console.WriteLine(number + " Khong phai so nguyen to");
+        }
+        else
+        {
+            int i = 2;
+            bool check = true;
+            while (i <= Math.Sqrt(number))
+            {
+                if (number % i == 0)
+                {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+            {
+                Console.WriteLine(number + " la so nguyen to");
+            }
+            else
+            {
+                Console.WriteLine(number + " khong phai la so nguyen to");
+            }
+        }
+        
+    }
+}
+
+
+
